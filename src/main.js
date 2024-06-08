09de483d1258f8656ae368d15b85bdbe0d1f1d94
ryoTakayanagi <r-takayanag_i@outlook.com>
2024-06-08 14:59:26 +0900

    import and export

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-const ConstantsClass = require('./constants.js');
-const Constants = new ConstantsClass();
+/* eslint-disable eqeqeq */
+/* eslint-disable no-undef */
+/**
+ * Copyright 2024 takayanag-i
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Constants } from './constants.js';
 
 const spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
 const masterSheet = spreadSheet.getSheetByName(Constants.SHEET.MASTER); // マスターシート
@@ -151,7 +168,7 @@ const processFormData = (formUrl, startCol, numCols) => {
 };
 
 // シートに値を設定するメイン関数
-const main = () => {
+export function main() {
   const formData = masterSheet
     .getRange(
       Constants.MASTER_RANGE.FORM_DATA_START_ROW,
@@ -177,4 +194,4 @@ const main = () => {
       .setValues(processedData);
     startCol += questionCount;
   }
-};
+}
